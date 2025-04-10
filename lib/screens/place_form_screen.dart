21d038520fb21f40b41f24cca206e58b5d798a37
Karini Sousa <131774110+Karini2037@users.noreply.github.com>
2025-04-09 22:16:03 -0300

    afezendo um projetinho de tirar foto e salvar imagens

---
+++ lib/screens/place_form_screen.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:great_places/widgets/image_input.dart';
+
+class PlaceFormScreen extends StatefulWidget {
+  const PlaceFormScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PlaceFormScreen> createState() => _PlaceFormScreenState();
+}
+
+class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+  final List<File> _images = [];
+
+  void _addImage(File pickedImage) {
+    setState(() {
+      _images.add(pickedImage);
+    });
+  }
+
+  void _submitForm() {
+    if (_titleController.text.isEmpty || _images.isEmpty) return;
+    //salva os dados
+  }
+
+  void _openCamera() {
+    //Mostra novamente o ImageInput ao clicar em "Avançar"
+    showBottomSheet(context: context,
+     builder: (_) => ImageInput(onSelectImage: _addImage),)
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Novo Lugar'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(
+                        labelText: 'Título',
+                      ),
+                      const SizedBox(height: 10),
+                      ElevatedButton.icon()
+                    ),
+                    const SizedBox(height: 10),
+                    const ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.add),
+            label: const Text('Adicionar'),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.black, backgroundColor: Theme.of(context).colorScheme.secondary,
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+            onPressed: _submitForm,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageInput extends StatefulWidget {
+
+  final void Function(File) onSelectImage;
+  const ImageInput({Key? key, required this.onSelectImage}) : super(key: key);
+
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+
+  _takePicture() async {
+    final ImagePicker picker = ImagePicker();
+    XFile imageFile = await picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    ) as XFile;
+
+    if (imageFile == null) return;
+
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+
+    // Envia a imagem para o pai  que é o PlaceFormScreen
+    widget.onSelectImage(_storedImage!);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 180,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          alignment: Alignment.center,
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : const Text('Nenhuma imagem!'),
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+          child: TextButton.icon(
+            icon: const Icon(Icons.camera),
+            label: const Text('Tirar Foto'),
+            onPressed: _takePicture,
+          ),
+        ),
+      ],
+    );
+  }
+}
